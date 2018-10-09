21b0ae5311779a7b06520597eaaca641e3fa4aee
arunkumar <java.aarunn@gmail.com>
2018-10-09 07:25:25 +0100

    Refactored the Spring boot application name

--- src/main/java/com/mojtest/Application.java
+++ src/main/java/com/mojtest/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MojtestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MojtestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
