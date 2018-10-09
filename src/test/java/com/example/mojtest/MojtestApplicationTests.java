be7e092ff466a653062be1b88ca9d9908081f3c3
arunkumar <java.aarunn@gmail.com>
2018-10-09 05:17:40 +0100

    Initialising an empty spring boot maven project

---
+++ src/test/java/com/example/mojtest/MojtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mojtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MojtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
